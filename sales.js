cf7b40c19aee99a2d4dc44b3a5f8d5f87d633494
Unknown <lnguyen34@mail.csuchico.edu>
2017-03-30 19:33:31 -0700

    lab done

--- sales.js
+++ sales.js
@@ -57,3 +57,14 @@ function getCookie(cname) {
     }
     return "";
 }
+
+function calculateReceipt()
+{
+  var receiptSubtotal = getCookie("preTax");
+  receiptSubtotal = Number(receiptSubtotal);
+  var receiptTax = receiptSubtotal * 0.075;
+  var receiptTotal = receiptSubtotal + receiptTax;
+  document.getElementById("sub").innerHTML = asCurrency(receiptSubtotal);
+  document.getElementById("tax").innerHTML = asCurrency(receiptTax);
+  document.getElementById("tot").innerHTML = asCurrency(receiptTotal);
+}
